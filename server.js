5295b18e670a32113fbfc3912f80a11ab1516d17
Raji <rajeswarim0112@gmail.com>
2025-01-19 20:18:46 +0530

    update backend

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 
 const db = mysql.createConnection({
